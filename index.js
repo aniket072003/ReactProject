0cdcc0b8a3e7288e5fbd91735f3fb8486fb9a3e1
aniket072003 <153921163+aniket072003@users.noreply.github.com>
2024-04-19 15:25:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Here the challenge is to create the react components and achive the same output using app.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
